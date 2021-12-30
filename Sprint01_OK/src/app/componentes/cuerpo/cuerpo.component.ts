f5e9c317a2523faf25199268a64c92ba49d6084e
Geovanizi <gebogo-1@hotmail.com>
2021-12-29 23:49:13 -0500

    Sprint 01
    Sprint 01

---
+++ Sprint01_OK/src/app/componentes/cuerpo/cuerpo.component.ts
@@ -0,0 +1,129 @@
+import { ARREGLO_CARROS } from './../../mocks/carro.mock';
+import { Carro } from './../../modelos/carro.model';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-cuerpo',
+  templateUrl: './cuerpo.component.html',
+  styleUrls: ['./cuerpo.component.css'],
+})
+export class CuerpoComponent implements OnInit {
+  public titulo: string;
+  public arregloCarros: Carro[];
+  public carroSeleccionado: Carro;
+
+  // ********************Propiedades Modal y Mensajes:
+  public modalRef:BsModalRef;
+  public modalTitulo:string;
+  public modalCuerpo: string;
+  public modalContenido: string;
+
+  public parametrosMensaje: any;
+
+  // nyeccion de dependencias en el constructor(miModal y miMensaje)
+  constructor(public miModal:BsModalService, public miMensaje:ToastrService) {
+    this.titulo = 'Listado de carros';
+    this.arregloCarros = ARREGLO_CARROS;
+    this.carroSeleccionado = this.inicializarCarro();
+
+    let temporal: any;
+    this.modalRef = temporal;
+    this.modalTitulo = "";    
+    this.modalContenido = "";
+    this.modalCuerpo = ""; 
+    
+    this.parametrosMensaje= {
+      closeButton: true,
+      enableHtml: true,
+      progressBar: true,
+      timeout: 7000,
+      positionClass: 'toast-top-center',
+    };
+  }
+
+  ngOnInit(): void {}
+
+  //************************ Metodos obligatorios:
+  public inicializarCarro(): Carro {
+    this.carroSeleccionado = new Carro(0, '', '', 0, '', '');
+    return this.carroSeleccionado;
+  }
+
+  public seleccionarCarro(objCarro: Carro): void {
+    this.carroSeleccionado = objCarro;
+  }
+
+  //************************ Lógica del negocio:
+  public crearCarro(): void {
+    
+    if(this.datosValidos()){
+      this.carroSeleccionado.codigo = this.arregloCarros.length + 1;
+    this.arregloCarros.push(this.carroSeleccionado);
+    this.inicializarCarro();
+    }
+  }
+
+  public actualizarCarro(): void {    
+    if(this.datosValidos()){
+      this.inicializarCarro();
+    }
+  }
+
+  public borrarCarro(miCarro:Carro): void {   
+      this.arregloCarros = this.arregloCarros.filter(
+        (objetoCarro) => objetoCarro != miCarro
+      );
+      this.inicializarCarro();    
+  }
+
+  public seleccionarFoto(caja: any): any {
+    if (!caja.target.files[0] || caja.target.files[0].length == 0) {
+      return;
+    }
+
+    const tipo = caja.target.files[0].type;
+    if (tipo.match(/image\/*/) == null) {
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.readAsDataURL(caja.target.files[0]);
+    reader.onload = () => {
+      let temporal: any;
+      temporal = reader.result;
+      this.carroSeleccionado.fotoBase64 = temporal;
+      this.carroSeleccionado.nombreFoto = caja.target.files[0].name;
+    };
+  }
+
+  public datosValidos(): boolean{
+    if(this.carroSeleccionado.placa == '' ||
+    this.carroSeleccionado.marca == '' ||
+    this.carroSeleccionado.modelo == 0){
+    this.miMensaje.error('No se puede crear el registro del carro sin llenar los campos', 'Advertencia',this.parametrosMensaje);
+      return false;
+    }
+    return true;
+  } 
+
+  // ******************Métodos para ventanas Modales:
+  public abrirModal(plantilla:TemplateRef<any>, objCarro:Carro): void{
+    this.carroSeleccionado = objCarro;
+    this.modalRef = this.miModal.show(plantilla,{ class:'modal-md'});
+    this.modalTitulo = 'Advertencia';
+    this.modalCuerpo = '¿Realmente quieres eliminar el carro?';
+    this.modalContenido = objCarro.placa + ' '+objCarro.marca + ' '+ objCarro.modelo;
+  }
+
+  public btnCancelar(): void{
+    this.modalRef.hide();
+  }
+
+  public btnEliminar(): void{
+    this.borrarCarro(this.carroSeleccionado);
+    this.btnCancelar();
+
+  }
+}
---
+++ Sprint01_OK/src/app/mocks/carro.mock.ts
@@ -0,0 +1,13 @@
+import { Carro } from './../modelos/carro.model';
+
+export let ARREGLO_CARROS: Array<Carro> = [
+  new Carro(1, 'BDG-789', 'Cummins', 2021, '', ''),
+  new Carro(2, 'ABC-123', 'Porsche', 2020, '', ''),
+  new Carro(3, 'IRY-155', 'Chevrolet', 2019, '', ''),
+  new Carro(4, 'PTJ-988', 'Audi', 2022, '', ''),
+  new Carro(5, 'AYT-966', 'Renault', 2020, '', ''),
+  new Carro(6, 'PIC-552', 'Rols-Royce', 2022, '', ''),
+  new Carro(7, 'ABZ-159', 'Daewoo', 2020, '', ''),
+  new Carro(8, 'ADW-111', 'Hino', 2021, '', ''),
+  new Carro(9, 'AXY-991', 'SsangYyong', 2021, '', ''),
+];
